d8738b77e71c592103adddd7c21b51f5996d2b27
Eduardo Kenji Hasegawa de Freitas <kenjihasegawa@live.com>
2019-11-20 02:31:56 -0300

    Initial Project Files

---
+++ GuestCheckApp/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestCheckApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/Product
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ GuestCheckApp/Tables/GuestCheckProduct.sql
@@ -0,0 +1,8 @@
+﻿IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='tblGuestCheckProduct' and xtype='U')
+	CREATE TABLE tblGuestCheckProduct (      
+		GuestCheckProductID int IDENTITY(1,1) NOT NULL PRIMARY KEY
+		,GuestCheckID int NOT NULL
+		,ProductID int NOT NULL
+		,ProductQty int NOT NULL
+	)      
+GO
