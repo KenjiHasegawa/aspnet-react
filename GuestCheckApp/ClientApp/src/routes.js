a444e85fee4bd14530f736359109f4ca1734f915
Eduardo Kenji Hasegawa de Freitas <kenjihasegawa@live.com>
2019-11-20 09:33:32 -0300

    WIP Front React

---
+++ GuestCheckApp/ClientApp/src/routes.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var react_router_dom_1 = require("react-router-dom");
+var Layout_1 = require("./components/Layout");
+var Home_1 = require("./components/Home");
+var FetchProduct_1 = require("./components/FetchProduct");
+exports.routes = React.createElement(Layout_1.Layout, null,
+    React.createElement(react_router_dom_1.Route, { exact: true, path: '/', component: Home_1.Home }),
+    React.createElement(react_router_dom_1.Route, { path: '/fetchproduct', component: FetchProduct_1.FetchProduct }));
+//# sourceMappingURL=routes.js.map
---
+++ GuestCheckApp/ClientApp/src/routes.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+import { Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchProduct } from './components/FetchProduct';
+export const routes = <Layout>
+    <Route exact path='/' component={Home} />
+    <Route path='/fetchproduct' component={FetchProduct} />
+</Layout>;
