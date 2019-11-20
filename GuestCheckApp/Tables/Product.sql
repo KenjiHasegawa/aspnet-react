b18457a3de9aded9bff2b81d102257ed094752d6
Eduardo Kenji Hasegawa de Freitas <kenjihasegawa@live.com>
2019-11-20 03:00:21 -0300

    Update Product.sql
    Adding products examples

--- GuestCheckApp/Tables/Product.sql
+++ GuestCheckApp/Tables/Product.sql
@@ -6,3 +6,15 @@
 		,ProductType VARCHAR(10) NOT NULL CHECK (ProductType IN('drink','food','dessert')) 
 	)      
 GO      
+
+IF EXISTS (SELECT * FROM sysobjects WHERE name='tblProduct' and xtype='U')
+INSERT INTO tblProduct
+(ProductName, ProductDescription, ProductType)
+VALUES
+('Coke', 'Better than pepsi', 'drink'),
+('Pepsi', 'Worst than Coke', 'drink'),
+('Dr. Pepper', 'Unusual', 'drink'),
+('Steak', '1kg', 'food'),
+('Meat Stew', 'Smells like heaven', 'food'),
+('Wine Balls', 'No Alcohol', 'dessert')
+GO
